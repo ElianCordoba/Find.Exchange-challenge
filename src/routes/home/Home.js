d2efc4850bd5607f7ce169aba235a54cdf552a40
Elian Cordoba <elian@whyline.com>
2018-11-17 12:08:45 -0300

    Initial Form integration to Home

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -7,39 +7,37 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import React, { PropTypes } from 'react';
-import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import s from './Home.css';
+import React from 'react';
+import Modal from '../../components/Modal/Modal';
+import Form from '../../components/Form/Form';
 
 class Home extends React.Component {
-  static propTypes = {
-    news: PropTypes.arrayOf(PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      link: PropTypes.string.isRequired,
-      content: PropTypes.string,
-    })).isRequired,
-  };
+  constructor() {
+    super();
+
+    this.state = {
+      modalOpen: false,
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      modalOpen: !this.state.modalOpen,
+    });
+  }
 
   render() {
     return (
-      <div className={s.root}>
-        <div className={s.container}>
-          <h1>React.js News</h1>
-          <ul className={s.news}>
-            {this.props.news.map((item, index) => (
-              <li key={index} className={s.newsItem}>
-                <a href={item.link} className={s.newsTitle}>{item.title}</a>
-                <span
-                  className={s.newsDesc}
-                  dangerouslySetInnerHTML={{ __html: item.content.substring(0, 100) }}
-                />
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div>
+        <button onClick={this.toggleModal}>Open Modal</button>
+        <Modal show={this.state.modalOpen}>
+          <Form />
+        </Modal>
       </div>
     );
   }
 }
 
-export default withStyles(s)(Home);
+export default Home;
