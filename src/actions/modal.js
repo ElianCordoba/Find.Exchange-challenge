d0db8237d8b0fc40a8d2ba62c0e04ab8287a93d9
Elian Cordoba <elian@whyline.com>
2018-11-22 21:07:10 -0300

    Added Modal's files to redux state

---
+++ src/actions/modal.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+
+import { MODAL_TOGGLE } from '../constants';
+
+export function modalToggle() {
+  return {
+    type: MODAL_TOGGLE,
+  };
+}
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 
 export const SET_RUNTIME_VARIABLE = 'SET_RUNTIME_VARIABLE';
+
+export const MODAL_TOGGLE = 'MODAL_TOGGLE';
